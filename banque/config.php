6d616c79965710f436b24085af3fe2d8ea15c22d
20syldev <git@sylvain.pro>
2025-02-09 01:07:36 +0100

    MEDIUM: php: Added reset every 10min

--- banque/config.php
+++ banque/config.php
@@ -1,17 +1,15 @@
 <?php
 // Fichier config.php
 
-if (!isset($_SESSION['comptes'])) {
 include 'modules/session.php';
+
+if (!isset($_SESSION['created']) || (time() - $_SESSION['created'] > 600)) {
     $_SESSION['comptes'] = [
         'c001' => ['nom' => 'Keger', 'prenom' => 'Alice', 'solde' => 4000],
         'c004' => ['nom' => 'Ruel', 'prenom' => 'Martine', 'solde' => 2500],
         'c005' => ['nom' => 'Erone', 'prenom' => 'Quentin', 'solde' => 1500],
         'c006' => ['nom' => 'Erone', 'prenom' => 'Anne', 'solde' => 2000],
     ];
-}
-
-if (!isset($_SESSION['actions'])) {
     $_SESSION['actions'] = [
         ['numero' => 'c001', 'type' => 'CB', 'montant' => -80],
         ['numero' => 'c001', 'type' => 'CHQ', 'montant' => -200],
@@ -22,4 +20,5 @@ if (!isset($_SESSION['actions'])) {
         ['numero' => 'c006', 'type' => 'VIR', 'montant' => 1000],
         ['numero' => 'c006', 'type' => 'CB', 'montant' => -500],
     ];
+    $_SESSION['created'] = time();
 }
