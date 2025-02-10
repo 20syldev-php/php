35535f1a7d32b9c48b0f1386c6ba8f3dfd0b8de7
20syldev <git@sylvain.pro>
2025-02-10 14:28:50 +0100

    MEDIUM: php: Removed default values

--- banque/config.php
+++ banque/config.php
@@ -4,21 +4,7 @@
 include 'modules/session.php';
 
 if (!isset($_SESSION['created']) || (time() - $_SESSION['created'] > 600)) {
-    $_SESSION['comptes'] = [
-        'c001' => ['nom' => 'Keger', 'prenom' => 'Alice', 'solde' => 4000],
-        'c004' => ['nom' => 'Ruel', 'prenom' => 'Martine', 'solde' => 2500],
-        'c005' => ['nom' => 'Erone', 'prenom' => 'Quentin', 'solde' => 1500],
-        'c006' => ['nom' => 'Erone', 'prenom' => 'Anne', 'solde' => 2000],
-    ];
-    $_SESSION['actions'] = [
-        ['numero' => 'c001', 'type' => 'CB', 'montant' => -80],
-        ['numero' => 'c001', 'type' => 'CHQ', 'montant' => -200],
-        ['numero' => 'c001', 'type' => 'CB', 'montant' => -20],
-        ['numero' => 'c001', 'type' => 'VIR', 'montant' => 2500],
-        ['numero' => 'c004', 'type' => 'CB', 'montant' => -80],
-        ['numero' => 'c004', 'type' => 'CB', 'montant' => -20],
-        ['numero' => 'c006', 'type' => 'VIR', 'montant' => 1000],
-        ['numero' => 'c006', 'type' => 'CB', 'montant' => -500],
-    ];
+    $_SESSION['comptes'] = [];
+    $_SESSION['actions'] = [];
     $_SESSION['created'] = time();
 }
